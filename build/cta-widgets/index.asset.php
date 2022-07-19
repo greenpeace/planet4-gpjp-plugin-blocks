87ab6d5d26491886dff4615be1d5d5843293a759
tkhg <28704185+tkhg@users.noreply.github.com>
2022-07-19 15:10:16 +0800

    fix: donation img not shown on mobile

--- build/cta-widgets/index.asset.php
+++ build/cta-widgets/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-autop', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '714c2953b3256f64c0aa');
+<?php return array('dependencies' => array('react', 'wp-autop', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '4d478d2f80d02425d032');
