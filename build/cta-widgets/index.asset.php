2b14fd337688f465727d8de56a80545f9a2f75cf
tkhg <28704185+tkhg@users.noreply.github.com>
2022-07-20 00:32:42 +0800

    fix: layout

--- build/cta-widgets/index.asset.php
+++ build/cta-widgets/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-autop', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '4d478d2f80d02425d032');
+<?php return array('dependencies' => array('react', 'wp-autop', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '7d01b52c6f3aad988fac');
